f418c87bb95638fdb70fd0a8bf6481e520dcb208
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-04-30 12:02:26 +0900

    イベントリスナー変更前

--- pages/random_name_app.tsx
+++ pages/random_name_app.tsx
@@ -1,4 +1,15 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import styles from "@/styles/random_name_app.module.css";
+
+// TODO: なぜかフォーカスが当たり、エンターキーを押すための範囲が狭い。
+// TODO 確定した名前がすぐ消える
+// TODO 名前を大きくする。
+// TODO 名前を追加するか選択できるように
+
+// cssのための関数
+function Layout({ children }) {
+  return <div className={styles.container}>{children}</div>;
+}
 
 let names = [
   "山田　太郎",
@@ -25,12 +36,24 @@ let names = [
 names = names.map((name) => name.replace(/[\s　]/g, ""));
 
 const Bingo: React.FC = () => {
-  const [selectedName, setSelectedName] = useState<string | null>(null);
+  const [selectedName, setSelectedName] = useState<string | null>(null); //確定した名前
+  const [IsShowingName, setIsShowingName] = useState<boolean>(false);
   const [nameConfirmed, setNameConfirmed] = useState<boolean>(false);
-  const [showNames, setShowNames] = useState<boolean>(false);
-  const [selectedNameList, setSelectedNameList] = useState<string[]>([]);
-  const [remainingNames, setRemainingNames] = useState<string[]>(names);
+  const [showRandomName, setShowRandomName] = useState<boolean>(false);
+  const [selectedNameList, setSelectedNameList] = useState<string[]>([]); //選択済みのリスト
+  const [remainingNames, setRemainingNames] = useState<string[]>(names); //まだ選択されていない名前のリスト
   const bingoRef = useRef<HTMLDivElement>(null);
+  const nameDisplay = useRef<HTMLDivElement>(null);
+
+  const handleResize = useCallback(() => {
+    const longestName = remainingNames.reduce((longest, name) =>
+      name.length > longest.length ? name : longest
+    );
+    const fontSize = Math.floor((window.innerWidth * 0.8) / longestName.length);
+    if (nameDisplay.current) {
+      nameDisplay.current.style.fontSize = `${fontSize}px`;
+    }
+  }, [remainingNames]);
 
   useEffect(() => {
     if (bingoRef.current) {
@@ -39,88 +62,144 @@ const Bingo: React.FC = () => {
   }, []);
 
   useEffect(() => {
-    let intervalId: NodeJS.Timeout;
+    handleResize();
+    window.addEventListener("resize", handleResize);
 
-    if (showNames) {
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [handleResize]);
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+    // ランダムに名前を表示する担当
+    if (showRandomName) {
+      //showRandomName ステートが true に設定されたときに発動
       intervalId = setInterval(() => {
         const remainingNamesCount = remainingNames.length;
         if (remainingNamesCount === 0) {
-          setShowNames(false);
+          //まだ選択されていない名前のリストが空になったら
+          setShowRandomName(false);
         } else {
           const randomIndex = Math.floor(Math.random() * remainingNamesCount);
           const selectedName = remainingNames[randomIndex];
           setSelectedName(selectedName);
         }
-      }, 100);
+      }, 60);
     }
-
     return () => {
       clearInterval(intervalId);
     };
-  }, [showNames, remainingNames]);
-
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
+  }, [showRandomName, remainingNames]);
+  // おそらく始まり
+  const startNameDisplay = useCallback((event: KeyboardEvent) => {
     if (event.key === "Enter") {
       event.preventDefault();
       if (!nameConfirmed) {
-        setShowNames(!showNames);
+        setShowRandomName(!showRandomName);
       } else {
         setNameConfirmed(false);
         setSelectedName(null);
-        setShowNames(false);
+        setShowRandomName(false);
       }
     }
-  };
+  }, []);
 
-  const handleConfirmName = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    if (event.key === "Enter" && selectedName) {
-      event.preventDefault();
-      setNameConfirmed(true);
-      setSelectedNameList([...selectedNameList, selectedName]);
-      setRemainingNames(remainingNames.filter((name) => name !== selectedName));
-    }
-    if (remainingNames.length === 0) {
-      setShowNames(false);
-    }
-  };
+  // 止まる
+  const stopNameDisplay = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === "Enter" && selectedName) {
+        event.preventDefault();
+        setNameConfirmed(true);
+        const shouldRemove = confirm(
+          `${selectedName}をリストから削除しますか？`
+        );
+        if (shouldRemove) {
+          setSelectedNameList([...selectedNameList, selectedName]); //ここで選択済みリストに追加
+          setRemainingNames(
+            remainingNames.filter((name) => name !== selectedName)
+          );
+          handleResize();
+        }
+      }
+      if (remainingNames.length === 0) {
+        setShowRandomName(false);
+      }
+    },
+    [selectedName, remainingNames]
+  );
+
+  // グローバルなイベントリスナーを設定
+  useEffect(() => {
+    window.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        if (IsShowingName) {
+          stopNameDisplay();
+        } else {
+          startNameDisplay();
+        }
+      }
+    });
+    window.addEventListener("keyup", stopNameDisplay);
+
+    return () => {
+      window.removeEventListener("keydown", startNameDisplay);
+      window.removeEventListener("keyup", stopNameDisplay);
+    };
+  }, [startNameDisplay, stopNameDisplay]);
+  // useEffect(() => {
+  //   window.addEventListener("keydown", startNameDisplay);
+  //   window.addEventListener("keyup", stopNameDisplay);
+
+  //   return () => {
+  //     window.removeEventListener("keydown", startNameDisplay);
+  //     window.removeEventListener("keyup", stopNameDisplay);
+  //   };
+  // }, [startNameDisplay, stopNameDisplay]);
 
   return (
-    <div
-      ref={bingoRef}
-      tabIndex={0}
-      onKeyDown={handleKeyPress}
-      onKeyUp={handleConfirmName}
-    >
+    <Layout>
       <div>
-        <h2>Names List:</h2>
+        <div>
+          <h2>Names List:</h2>
 
-        {remainingNames.map((name) => (
-          <span key={name}>{name} </span>
-        ))}
-      </div>
-      <div>
-        <h2>Selected Name:</h2>
-        {selectedName && <p>{selectedName}</p>}
-        {!nameConfirmed && <p>Press Enter to start/stop the name selection</p>}
-      </div>
+          {remainingNames.map((name) => (
+            <span key={name}>{name} </span>
+          ))}
+        </div>
+        <div>
+          <h2>Selected Name:</h2>
+          {
+            selectedName && (
+              <div className="nameDisplay" ref={nameDisplay}>
+                {selectedName}
+              </div>
+            ) /* selectedNameがある場合のみ表示 */
+          }
+          <br></br>
+          {!nameConfirmed && (
+            <p>Press Enter to start/stop the name selection</p>
+          )}
+        </div>
 
-      {selectedNameList.length > 0 && (
-        <div>
-          <h2>Selected Name List:</h2>
-          <p>{selectedNameList.join(" ")}</p>
-        </div>
-      )}
-      {selectedName && !nameConfirmed && (
-        <div>
-          <p>Press Enter to confirm the selected name</p>
-        </div>
-      )}
-      {remainingNames.length === 0 && (
-        <div>
-          <p>All names have been selected</p>
-        </div>
-      )}
-    </div>
+        {selectedNameList.length > 0 && (
+          <div>
+            <h2>Selected Name List:</h2>
+            <p>{selectedNameList.join(" ")}</p>
+          </div>
+        )}
+        {selectedName && !nameConfirmed && (
+          <div>
+            <p>Press Enter to confirm the selected name</p>
+          </div>
+        )}
+        {remainingNames.length === 0 && (
+          <div>
+            <p>All names have been selected</p>
+          </div>
+        )}
+      </div>
+    </Layout>
   );
 };
 

e78e3cd2816dc8a9b6fc631638b7c72047fb12b8
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-05-11 23:20:14 +0900

    リストの編集画面を作る

---
+++ components/EditListForm/EditListForm.tsx
@@ -0,0 +1,12 @@
+import { v4 as uuidv4 } from "uuid";
+
+//新規リスト作成
+const EditListForm = ({ lists, setLists }) => {
+  //新規リスト作成
+
+  //ここに関数とかいろいろ書く
+
+  return <div className="flex items-center">{/*こんな感じで書いていく */}</div>;
+};
+
+export default EditListForm;

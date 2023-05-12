7fabdf6f3b25074418342f406af41d2fe3d38578
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-05-12 10:33:20 +0900

    名前変更

--- utils/fetchListsFromLocalStorage.ts
+++
@@ -1,5 +0,0 @@
-export const fetchListsFromLocalStorage = () => {
-  const storedLists = localStorage.getItem("listNames");
-  const parsedLists = storedLists ? JSON.parse(storedLists) : [];
-  return Array.isArray(parsedLists) ? parsedLists : [];
-};

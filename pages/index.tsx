feaf48f6f91d20070bca1693218377869a3479f3
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-05-19 13:10:40 +0900

    min-h-screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,15 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <section>
-        <h1>yur Appへようこそ</h1>
-        <p>とにかく大きいランダムネームアプリ</p>
-        <Link href={`/random_name_app/home`} className={""}>
-          スタート
-        </Link>
-      </section>
+      <div className="min-h-screen">
+        <section>
+          <h1>yur Appへようこそ</h1>
+          <p>とにかく大きいランダムネームアプリ</p>
+          <Link href={`/random_name_app/home`} className={"hover:bg-blue-200"}>
+            クリック
+          </Link>
+        </section>
+      </div>
     </>
   );
 }

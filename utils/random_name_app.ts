58609306727b63d3a32d1ca034108b2352c921f8
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-05-21 13:02:41 +0900

    どこでも関数使えるようにした

--- utils/random_name_app.ts
+++ utils/random_name_app.ts
@@ -3,3 +3,14 @@ export const fetchListsFromLocalStorage = () => {
   const parsedLists = storedLists ? JSON.parse(storedLists) : [];
   return Array.isArray(parsedLists) ? parsedLists : [];
 };
+
+export const isSmartPhone = () => {
+  if (
+    window.matchMedia &&
+    window.matchMedia("(max-device-width: 640px)").matches
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};

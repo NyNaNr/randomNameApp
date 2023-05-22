46467a3077e823957241daa1c73075d0ac437f7a
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-05-22 20:38:40 +0900

    Open,Closeのボタン調整

--- components/Chatbar/OpenCloseButton.tsx
+++ components/Chatbar/OpenCloseButton.tsx
@@ -8,8 +8,8 @@ export const CloseSidebarButton = ({ onClick }: Props) => {
   return (
     <>
       <button
-        className={`fixed top-5 left-[270px]
-        z-50 h-7 w-7 hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:left-[270px] sm:h-8 sm:w-8 sm:text-neutral-700`}
+        className={`fixed top-24 left-[270px]
+        z-50 h-7 w-7 hover:text-gray-400 text-white dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:left-[270px] sm:h-8 sm:w-8 sm:text-neutral-700`}
         onClick={onClick}
       >
         <ArrowBarLeft />
@@ -21,8 +21,8 @@ export const CloseSidebarButton = ({ onClick }: Props) => {
 export const OpenSidebarButton = ({ onClick }: Props) => {
   return (
     <button
-      className={`fixed top-2.5 left-2
-       z-50 h-7 w-7 text-white hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:left-2
+      className={`fixed top-24 left-2
+       z-50 h-7 w-7 text-black hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-0.5 sm:left-2
        sm:h-8 sm:w-8 sm:text-neutral-700`}
       onClick={onClick}
     >

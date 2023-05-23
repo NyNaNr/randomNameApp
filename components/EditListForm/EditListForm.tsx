78bb6e48d7ce15f89f731ded2a087074d800b3e5
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-05-23 10:58:03 +0900

    マージンを追加

--- components/EditListForm/EditListForm.tsx
+++ components/EditListForm/EditListForm.tsx
@@ -263,7 +263,7 @@ const EditListForm = ({
           </div>
         ) : (
           <div className="flex items-center justify-center min-h-screen">
-            <div className="mx-auto flex w-[300px] flex-col justify-center space-y-6 lg:w-[600px]">
+            <div className="mx-auto flex w-[300px] flex-col justify-center mt-10 space-y-6 lg:w-[600px]">
               <div className="text-center text-4xl lg:text-5xl font-bold text-black dark:text-white">
                 Welcome to{" "}
                 <span className="text-3xl lg:text-5xl">
@@ -283,14 +283,14 @@ const EditListForm = ({
                   color={"currentColor"}
                 />
               </div>
-              <div className="text-center text-gray-500 dark:text-gray-400">
+              <div className="text-center text-gray-500 dark:text-gray-400 ">
                 <div className="mb-2">
                   入力された名前データの保存期間は、基本的に365日、一部ブラウザで7日間です。
                 </div>
                 <div className="mb-2">
                   リスト名をクリックすると、そのリストのcookieの保存期間が更新・延長されます。
                 </div>
-                <div className="mb-2">
+                <div className="mb-10">
                   もしご自身でリストの保存期間を確認されたい場合は、「cookie
                   確認方法」などと検索してください。
                 </div>

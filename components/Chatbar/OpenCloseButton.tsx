a0a51cc218adb335927c76d11dc1768aea96f894
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-05-27 16:34:57 +0900

    位置PC時の微修正

--- components/Chatbar/OpenCloseButton.tsx
+++ components/Chatbar/OpenCloseButton.tsx
@@ -9,7 +9,7 @@ export const CloseSidebarButton = ({ onClick }: Props) => {
     <>
       <button
         className={`fixed top-20 left-[270px]
-        z-50 h-7 w-7 hover:text-gray-400 text-white dark:text-white dark:hover:text-gray-300 sm:top-16 sm:left-[270px] sm:h-8 sm:w-8 sm:text-neutral-700`}
+        z-50 h-7 w-7 hover:text-gray-400 text-white dark:text-white dark:hover:text-gray-300 sm:top-20 sm:left-[270px] sm:h-8 sm:w-8 sm:text-neutral-700`}
         onClick={onClick}
       >
         <ArrowBarLeft />
@@ -22,7 +22,7 @@ export const OpenSidebarButton = ({ onClick }: Props) => {
   return (
     <button
       className={`fixed top-20 left-2
-       z-50 h-7 w-7 text-black hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-16 sm:left-2
+       z-50 h-7 w-7 text-black hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-20 sm:left-2
        sm:h-8 sm:w-8 sm:text-neutral-700`}
       onClick={onClick}
     >

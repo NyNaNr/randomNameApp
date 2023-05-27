a1d45d525aa7d4b76293906ffcb62445f6d8dbf0
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-05-27 15:54:36 +0900

    CSS変更

--- components/EditListForm/Layout.tsx
+++ components/EditListForm/Layout.tsx
@@ -20,55 +20,53 @@ const Layout: React.FC<LayoutProps> = ({
 }) => (
   <React.Fragment>
     {selectedListId && (
-      <header className="header fixed top-0 w-full z-40 bg-white">
-        {isOpen ? (
-          <div
-            onClick={toggleOpen}
-            className="absolute top-0 left-0 z-10 h-full w-full bg-black opacity-70 sm:hidden"
-          ></div>
-        ) : (
-          ""
-        )}
-        <div className="text-black">
-          <p>保存可能な文字容量: {totalBytes}/4058 bytes</p>
-        </div>
-        {totalBytes > 4058 ? (
-          <div className="text-black animate-flash">
-            <p>文字数が保存容量を超えています。減らしてください。</p>
+      <header className="header fixed top-0 w-full z-1 bg-white ">
+        <div className="container relative">
+          {isOpen ? (
+            <div
+              onClick={toggleOpen}
+              className="absolute top-0 left-0 z-10 h-full w-full bg-black opacity-70 sm:hidden"
+            ></div>
+          ) : (
+            ""
+          )}
+          <div className="text-black">
+            <p>保存可能な文字容量: {totalBytes}/4058 bytes</p>
           </div>
-        ) : (
-          <div>
-            <br />
-          </div>
-        )}
-        <div className="text-black">合計 : {lineNumber}人</div>
-        {lineNumber < 2 ? (
-          <div className="text-black animate-flash">
-            <p>2人以上入力してください</p>
-          </div>
-        ) : (
-          <div>
-            <br />
-          </div>
-        )}
-
-        <div className="start-links relative">
-          {totalBytes > 4058 || lineNumber < 2 ? (
-            <div className="link-blocker absolute h-15 px-2 py-1 text-lg text-white font-semibold rounded z-20 cursor-not-allowed">
-              スタート
+          {totalBytes > 4058 ? (
+            <div className="text-black animate-flash">
+              <p>文字数が保存容量を超えています。減らしてください。</p>
             </div>
-          ) : null}
+          ) : (
+            <div></div>
+          )}
+          <div className="text-black">合計 : {lineNumber}人</div>
+          {lineNumber < 2 ? (
+            <div className="text-black animate-flash">
+              <p>2人以上入力してください</p>
+            </div>
+          ) : (
+            <div></div>
+          )}
 
-          <Link
-            href={`/random_name_app/id/${selectedListId}`}
-            className={`absolute h-15 z-10 shadow-lg px-2 py-1 bg-blue-400 text-lg text-white font-semibold rounded  hover:shadow-sm  ${
-              totalBytes > 4058 || lineNumber < 2
-                ? " bg-gray-500/50"
-                : "hover:bg-blue-500"
-            }`}
-          >
-            スタート
-          </Link>
+          <div className="start-links absolute bottom-0 right-0">
+            {totalBytes > 4058 || lineNumber < 2 ? (
+              <div className="link-blocker absolute h-15 px-2 py-1 text-lg text-white font-semibold rounded z-20 cursor-not-allowed">
+                スタート
+              </div>
+            ) : null}
+
+            <Link
+              href={`/random_name_app/id/${selectedListId}`}
+              className={`absolute h-15 z-10 shadow-lg px-2 py-1 bg-blue-400 text-lg text-white font-semibold rounded  hover:shadow-sm  ${
+                totalBytes > 4058 || lineNumber < 2
+                  ? " bg-gray-500/50"
+                  : "hover:bg-blue-500"
+              }`}
+            >
+              スタート
+            </Link>
+          </div>
         </div>
       </header>
     )}

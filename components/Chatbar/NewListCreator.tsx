af2bf37b6e9b7500844d34fcb870e5158b6d3c3d
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-05-27 20:20:51 +0900

    ボタンのネストを解消

--- components/Chatbar/NewListCreator.tsx
+++ components/Chatbar/NewListCreator.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect, ChangeEvent } from "react";
 import { fetchListsFromLocalStorage } from "../../utils/random_name_app";
+import { isMobile } from "../../utils/random_name_app";
 
 import Cookies from "js-cookie";
 
@@ -418,26 +419,24 @@ const NewListCreator: React.FC<NewListCreatorProps> = ({
                       onListClick(list.id);
                     }}
                   >
-                    <button className="min-w-[20px] p-1 text-neutral-400 hover:text-neutral-100">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="18"
-                        height="18"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        className="tabler-icon tabler-icon-trash"
-                      >
-                        <path d="M4 7l16 0"></path>
-                        <path d="M10 11l0 6"></path>
-                        <path d="M14 11l0 6"></path>
-                        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
-                        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
-                      </svg>
-                    </button>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="18"
+                      height="18"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      className="tabler-icon tabler-icon-trash"
+                    >
+                      <path d="M4 7l16 0"></path>
+                      <path d="M10 11l0 6"></path>
+                      <path d="M14 11l0 6"></path>
+                      <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+                      <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+                    </svg>
                   </button>
                 </div>
               );

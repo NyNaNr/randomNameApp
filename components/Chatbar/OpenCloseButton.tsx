793ed76a8484a2cf9560479509e822f26e798a72
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-05-28 13:07:26 +0900

    ボタンが目立つように光らせた

--- components/Chatbar/OpenCloseButton.tsx
+++ components/Chatbar/OpenCloseButton.tsx
@@ -9,7 +9,7 @@ export const CloseSidebarButton = ({ onClick }: Props) => {
     <>
       <button
         className={`fixed top-20 left-[270px]
-        z-50 h-7 w-7 hover:text-gray-400 text-white dark:text-white dark:hover:text-gray-300 sm:top-20 sm:left-[270px] sm:h-8 sm:w-8 sm:text-neutral-700`}
+        z-50 h-7 w-7 hover:text-gray-400 text-white dark:text-white dark:hover:text-gray-300 sm:top-20 sm:left-[270px] sm:h-8 sm:w-8 sm:text-neutral-700 animate-flash`}
         onClick={onClick}
       >
         <ArrowBarLeft />
@@ -23,7 +23,7 @@ export const OpenSidebarButton = ({ onClick }: Props) => {
     <button
       className={`fixed top-20 left-2
        z-50 h-7 w-7 text-black hover:text-gray-400 dark:text-white dark:hover:text-gray-300 sm:top-20 sm:left-2
-       sm:h-8 sm:w-8 sm:text-neutral-700`}
+       sm:h-8 sm:w-8 sm:text-neutral-700 animate-flash`}
       onClick={onClick}
     >
       <ArrowBarRight />

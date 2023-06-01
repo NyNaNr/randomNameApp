fdfa41ab217279b58757b796d7220a7ccecd1982
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-01 10:51:25 +0900

    google search console

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        {/* OGP settings */}
+        {/* OGP settings  */}
         <meta
           property="og:title"
           content="とにかく大きい ランダムネーム アプリ"

051afbe74cd69e828e3f056dccc61135298b4f0f
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-06-27 23:46:24 +0900

    initialFocusをyesに変更

--- components/Modals.tsx
+++ components/Modals.tsx
@@ -16,14 +16,14 @@ export default function Modals({
   content,
   onClick,
 }: ModalsProps) {
-  const cancelButtonRef = useRef(null);
+  const yesButtonRef = useRef(null);
 
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
         as="div"
         className="relative z-10"
-        initialFocus={cancelButtonRef}
+        initialFocus={yesButtonRef}
         static
         onClose={() => {}}
       >
@@ -75,6 +75,7 @@ export default function Modals({
 
                       onClick();
                     }}
+                    ref={yesButtonRef}
                   >
                     はい
                   </button>
@@ -82,7 +83,6 @@ export default function Modals({
                     type="button"
                     className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                     onClick={() => setOpen(false)}
-                    ref={cancelButtonRef}
                   >
                     いいえ
                   </button>

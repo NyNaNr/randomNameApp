d6f83e56c69008279aa828a4973ecae43cb7c4d2
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-09-12 06:07:38 +0900

    ユーザーオプションの位置ざっくり指定

--- components/EditListForm/Layout.tsx
+++ components/EditListForm/Layout.tsx
@@ -19,55 +19,69 @@ const Layout: React.FC<LayoutProps> = ({
 }) => (
   <React.Fragment>
     {selectedListId && (
-      <header className="header fixed top-0 w-full z-10 p-1 bg-white shadow-md">
-        {isOpen ? (
-          <div
-            onClick={toggleOpen}
-            className="absolute top-0 left-0 z-10 min-h-screen h-full w-full bg-black opacity-70 sm:hidden"
-          ></div>
-        ) : (
-          ""
-        )}
-        <div className="container">
-          <div className="text-black">
-            <p>保存可能な文字容量: {totalBytes}/4058 bytes</p>
-          </div>
-          {totalBytes > 4058 ? (
-            <div className="text-black animate-flash">
-              <p>文字数が保存容量を超えています。減らしてください。</p>
-            </div>
+      <div className="relative">
+        <header className="header fixed top-0 w-full z-10 p-1 bg-white shadow-md">
+          {isOpen ? (
+            <div
+              onClick={toggleOpen}
+              className="absolute top-0 left-0 z-10 min-h-screen h-full w-full bg-black opacity-70 sm:hidden"
+            ></div>
           ) : (
-            <div></div>
+            ""
           )}
-          <div className="text-black">合計 : {lineNumber}人</div>
-          {lineNumber < 2 ? (
-            <div className="text-black animate-flash">
-              <p>2人以上入力してください</p>
+          <div className="container">
+            <div className="text-black">
+              <p>保存可能な文字容量: {totalBytes}/4058 bytes</p>
             </div>
-          ) : (
-            <div></div>
-          )}
-
-          <div className="fixed start-links z-5 flex top-2 right-0 w-24">
-            {totalBytes > 4058 || lineNumber < 2 ? (
-              <div className="link-blocker absolute h-15 px-2 py-1 text-lg text-white font-semibold rounded z-20 cursor-not-allowed">
-                スタート
+            {totalBytes > 4058 ? (
+              <div className="text-black animate-flash">
+                <p>文字数が保存容量を超えています。減らしてください。</p>
               </div>
-            ) : null}
+            ) : (
+              <div></div>
+            )}
+            <div className="text-black">合計 : {lineNumber}人</div>
+            {lineNumber < 2 ? (
+              <div className="text-black animate-flash">
+                <p>2人以上入力してください</p>
+              </div>
+            ) : (
+              <div></div>
+            )}
 
-            <Link
-              href={`/random_name_app/id/${selectedListId}`}
-              className={`absolute h-15 z-10 shadow-lg px-2 py-1 bg-blue-400 text-lg text-white font-semibold rounded hover:shadow-sm  ${
-                totalBytes > 4058 || lineNumber < 2
-                  ? " bg-gray-500/50"
-                  : "hover:bg-blue-500"
-              }`}
-            >
-              スタート
-            </Link>
+            <div className="fixed start-links z-5 flex top-2 right-0 w-24">
+              {totalBytes > 4058 || lineNumber < 2 ? (
+                <div className="link-blocker absolute h-15 px-2 py-1 text-lg text-white font-semibold rounded z-20 cursor-not-allowed">
+                  スタート
+                </div>
+              ) : null}
+
+              <Link
+                href={`/random_name_app/id/${selectedListId}`}
+                className={`absolute h-15 z-10 shadow-lg px-2 py-1 bg-blue-400 text-lg text-white font-semibold rounded hover:shadow-sm  ${
+                  totalBytes > 4058 || lineNumber < 2
+                    ? " bg-gray-500/50"
+                    : "hover:bg-blue-500"
+                }`}
+              >
+                スタート
+              </Link>
+            </div>
           </div>
+        </header>
+        <div className="user-options absolute top-14 right-0 z-20">
+          <details className="rounded-lg shadow-md rounded-tl-none rounded-tr-none  bg-white transition-all duration-300 z-20 px-2 mr-4">
+            <summary className="">詳細設定</summary>
+            <p>
+              了解しました。ヘッダー内にその要素を入れずに、ヘッダーのすぐ右下に表示させる場合、ヘッダーとその要素が含まれる親要素（コンテナ）をrelativeで配置し、user-optionsをその親要素に対して絶対配置する方法が考えられます。
+              以下の手順でそれを実現できます。
+              ヘッダーとuser-optionsの両方を含む親要素（コンテナ）を作成し、そのコンテナにrelativeクラスを追加します。
+              user-optionsをコンテナの中に配置し、absoluteクラスを使用してコンテナの右下に配置します。
+              コードとしては以下のようになります：
+            </p>
+          </details>
         </div>
-      </header>
+      </div>
     )}
     {selectedListId ? (
       <div className="mt-24 flex-grow">{children}</div>
